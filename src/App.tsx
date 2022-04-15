a598a21cdcacf2f892f0484b22b0af5c5b8f56ab
Yuui Tanabe <yuui_21s1200343@nnn.ed.jp>
2022-04-16 01:00:56 +0900

    Initial

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import type { Component } from 'solid-js';
+
+import logo from './logo.svg';
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div class={styles.App}>
+      <header class={styles.header}>
+        <img src={logo} class={styles.logo} alt="logo" />
+        <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          class={styles.link}
+          href="https://github.com/facebook/react"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default App;
